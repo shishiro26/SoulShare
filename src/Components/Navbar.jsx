39e201d2b03ee3bcb6b6b8c85acd3fd4818d5bc6
[karthik] <[lcs2022026@iiitl.ac.in]>
2023-08-12 17:16:58 +0530

    added hover effect to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,50 +1,59 @@
 const Navbar = () => {
   return (
     <nav className="flex flex-col justify-between center max-w-[48px]">
-      <div className="Navbar_navList__mg4Oa">
-        <a
-          data-navbar-item="true"
-          className="Navbar_navLink__Havgb"
-          aria-current=""
-          href="/"
-        >
-          Home
-        </a>
-        <a
-          data-navbar-item="true"
-          className="Navbar_navLink__Havgb"
-          aria-current=""
-          href="/events/"
-        >
-         Donars
-        </a>
-        
-        <a
-          data-navbar-item="true"
-          className="Navbar_navLink__Havgb"
-          aria-current=""
-          href="/teams/"
-        >
-          Teams
-        </a>
-        <a
-          data-navbar-item="true"
-          className="Navbar_navLink__Havgb"
-          aria-current=""
-          href="/gallery/"
-        >
-         About
-        </a>
-        <a
-          data-navbar-item="true"
-          className="Navbar_navLink__Havgb"
-          aria-current=""
-          href="/contact/"
-        >
-          Contact
-        </a>
-        </div>
-          </nav>
+      <ul className="Navbar_navList__mg4Oa">
+        <li className="inline-block text-[25px] ml-[10px] px-[0px] py-[20px] list-none after:block after:content-[''] after:z-[-1] after:h-[10px] after:border-b-[1px] after:border-[#00FFFF] after:border-solid after:scale-x-[0] after:ease-in-out after:duration-300 after:transition-[transform] hover:after:scale-x-[1] hover:after:translate-y-[-15px] hover:after:translate-x-[14px] hover:after:bg-[#37FF8B] hover:after:z-[-1] hover:after:opacity-[0.3] hover:after:scale-x-[1] hover:after:translate-y-[-15px] hover:after:translate-x-[14px] hover:after:bg-[#37FF8B] hover:after:z-[-1] hover:after:opacity-[0.3]">
+          <a
+            data-navbar-item="true"
+            className="p-[16px] font-[500] text-sm no-underline relative transition-all hover:text-[#37FF8B]  z-[80]"
+            aria-current=""
+            href="/"
+          >
+            Home
+          </a>
+        </li>
+        <li className="inline-block text-[25px] ml-[10px] px-[0px] py-[20px] list-none after:block after:content-[''] after:z-[-1] after:h-[10px] after:border-b-[1px] after:border-[#00FFFF] after:border-solid after:scale-x-[0] after:ease-in-out after:duration-300 after:transition-[transform] hover:after:scale-x-[1] hover:after:translate-y-[-15px] hover:after:translate-x-[14px] hover:after:bg-[#37FF8B] hover:after:z-[-1] hover:after:opacity-[0.3] ">
+          <a
+            data-navbar-item="true"
+            className="p-[16px] font-[500] text-sm no-underline relative transition-all hover:text-[#37FF8B]  z-[80]"
+            aria-current=""
+            href="/"
+          >
+            Teams
+          </a>
+        </li>
+        <li className="inline-block text-[25px] ml-[10px] px-[0px] py-[20px] list-none after:block after:content-[''] after:z-[-1] after:h-[10px] after:border-b-[1px] after:border-[#00FFFF] after:border-solid after:scale-x-[0] after:ease-in-out after:duration-300 after:transition-[transform] hover:after:scale-x-[1] hover:after:translate-y-[-15px] hover:after:translate-x-[14px] hover:after:bg-[#37FF8B] hover:after:z-[-1] hover:after:opacity-[0.3] ">
+          <a
+            data-navbar-item="true"
+            className="p-[16px] font-[500] text-sm no-underline relative transition-all hover:text-[#37FF8B]  z-[80]"
+            aria-current=""
+            href="/"
+          >
+            Donars
+          </a>
+        </li>
+        <li className="inline-block text-[25px] ml-[10px] px-[0px] py-[20px] list-none after:block after:content-[''] after:z-[-1] after:h-[10px] after:border-b-[1px] after:border-[#00FFFF] after:border-solid after:scale-x-[0] after:ease-in-out after:duration-300 after:transition-[transform] hover:after:scale-x-[1] hover:after:translate-y-[-15px] hover:after:translate-x-[14px] hover:after:bg-[#37FF8B] hover:after:z-[-1] hover:after:opacity-[0.3] ">
+          <a
+            data-navbar-item="true"
+            className="p-[16px] font-[500] text-sm no-underline relative transition-all hover:text-[#37FF8B]  z-[80]"
+            aria-current=""
+            href="/"
+          >
+            About
+          </a>
+        </li>
+        <li className="inline-block text-[25px] ml-[10px] px-[0px] py-[20px] list-none after:block after:content-[''] after:z-[-1] after:h-[10px] after:border-b-[1px] after:border-[#00FFFF] after:border-solid after:scale-x-[0] after:ease-in-out after:duration-300 after:transition-[transform] hover:after:scale-x-[1] hover:after:translate-y-[-15px] hover:after:translate-x-[14px] hover:after:bg-[#37FF8B] hover:after:z-[-1] hover:after:opacity-[0.3] after:block after:content-[''] after:z-[-1] after:h-[10px] after:border-b-[1px] after:border-[#00FFFF] after:border-solid after:scale-x-[0] after:ease-in-out after:duration-300 after:transition-[transform]">
+          <a
+            data-navbar-item="true"
+            className="p-[16px] font-[500] text-sm no-underline relative transition-all hover:text-[#37FF8B]  z-[80]"
+            aria-current=""
+            href="/"
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
