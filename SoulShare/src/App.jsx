ca2308582f17f9b8985c2064534dc93cf549fe41
shishiro <dheervathshishiro@gmail.com>
2023-08-12 13:51:35 +0530

    Added tailwind to the project

--- SoulShare/src/App.jsx
+++ SoulShare/src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <h1 className="text-blue-600">This is the SoulShare</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
