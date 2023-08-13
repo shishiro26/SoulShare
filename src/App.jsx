4d68cc99f82b0d772c067beb9b69bfad347641f8
[karthik] <[lcs2022026@iiitl.ac.in]>
2023-08-13 18:06:33 +0530

    added card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import Card from "./Components/Card";
+import CardClothes from "./Components/CardClothes";
+import FoodCard from "./Components/FoodCard";
+import Clock from "./Components/Timer/Clock";
+import Timer from "./Components/Timer/Timer";
 import ContactUs from "./Pages/ContactUs";
 import Login from "./Pages/LogIn";
 import SignUp from "./Pages/SignUp";
@@ -6,15 +9,18 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/contact" element={<ContactUs />} />
+    // <Routes>
+    //   <Route path="/signup" element={<SignUp />} />
+    //   <Route path="/login" element={<Login />} />
+    //   <Route path="/contact" element={<ContactUs />} />
 
-    </Routes>
-    // <div>
-    //   <Card/>
-    // </div>
+    // </Routes>
+    <div>
+      {/* <CardClothes/> */}
+      {/* <FoodCard/> */}
+      <Clock/>
+      <Timer/>
+    </div>
   );
 }
 
