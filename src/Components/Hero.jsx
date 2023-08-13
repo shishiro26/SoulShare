7f5540ce9968270b50370e95e7992112c5dc12b2
shishiro <dheervathshishiro@gmail.com>
2023-08-13 19:14:00 +0530

    added navbar to the homepage

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,41 +1,41 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import Navbar from '../Components/Navbar.jsx'
-import ShareSocials from '../Components/ShareSocials.jsx'
+// import { useState } from "react";
+// import { Dialog } from "@headlessui/react";
+// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+// import Navbar from "../Components/Navbar.jsx";
+// import ShareSocials from "../Components/ShareSocials.jsx";
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
+// const navigation = [
+//   { name: "Product", href: "#" },
+//   { name: "Features", href: "#" },
+//   { name: "Marketplace", href: "#" },
+//   { name: "Company", href: "#" },
+// ];
 
 export default function Landing() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  // const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className='flex flex-row'>
-      <Navbar />
+    <div className="">
       <div className="relative isolate px-6 pt-14 lg:px-8 w-[100%]">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true">
+          aria-hidden="true"
+        >
           <div
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-100 ring-1 ring-gray-900/10 hover:ring-gray-100/20">
-              Announcing our next round of funding.{' '}
+              Announcing our next round of funding.{" "}
               <a href="#" className="font-semibold text-green-500 ">
                 <span className="absolute inset-0  " aria-hidden="true" />
-                Read more 
+                Read more
                 <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
@@ -45,8 +45,9 @@ export default function Landing() {
               Data to enrich your online business
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
@@ -55,7 +56,10 @@ export default function Landing() {
               >
                 Get started
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-green-500">
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-green-500"
+              >
                 Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
@@ -69,12 +73,11 @@ export default function Landing() {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
       </div>
-      <ShareSocials />
     </div>
-  )
+  );
 }
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import Hero from '../Components/Hero.jsx';
-import Feature from '../Components/Feature.jsx';
-import AboutUs from '../Components/AboutUs.jsx';
-import Testimonials from '../Components/Testimonials.jsx';
+import Hero from "../Components/Hero.jsx";
+import Feature from "../Components/Feature.jsx";
+import AboutUs from "../Components/AboutUs.jsx";
+import Testimonials from "../Components/Testimonials.jsx";
+import Navbar from "../Components/Navbar";
+import ShareSocials from "../Components/ShareSocials";
 
 const LandingPage = () => {
-    return ( 
-        <>
-            <Hero />
-            <Feature />        
-            <AboutUs />
-            <Testimonials />
-        </>
-     );
-}
- 
+  return (
+    <>
+      <div className="flex flex-row">
+        <Navbar />
+        <div>
+          <Hero />
+          <Feature />
+          <AboutUs />
+          <Testimonials />
+        </div>
+        <ShareSocials />
+      </div>
+    </>
+  );
+};
+
 export default LandingPage;
