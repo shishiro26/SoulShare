8c2595b07afd882905dddc190f777ce0eba30752
[karthik] <[lcs2022026@iiitl.ac.in]>
2023-08-14 16:08:10 +0530

    update aboutus

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -5,19 +5,19 @@ import { Typewriter } from "react-simple-typewriter";
 const people = [
   {
     name: "Shishiro Dheeravath",
-    role: "Co-Founder",
+    role: "Frontend Developer",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Chitta Sri Sankara Naga Pavana Saketha Ram",
-    role: "Co-Founder ",
+    role: "Backend Developer",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Kota",
-    role: "Co-Founder ",
+    role: "Frontend Developer",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
