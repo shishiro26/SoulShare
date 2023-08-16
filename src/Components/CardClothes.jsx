92acc0145e170d2e983f87e5553ecbc2699978b1
shishiro <dheervathshishiro@gmail.com>
2023-08-16 19:44:15 +0530

    added browse page with the cards

--- src/Components/CardClothes.jsx
+++ src/Components/CardClothes.jsx
@@ -1,7 +1,7 @@
 import TestImage from "../assets/pexels-rajesh-tp-1624487.jpg";
 import Heart from "../assets/Heart.svg";
 import { useState } from "react";
-const CardClothes = () => {
+const CardClothes = (props) => {
   const [heartColor, setHeartColor] = useState("none");
   const ChangeHeartColor = () => {
     if (heartColor == "none") {
@@ -67,7 +67,7 @@ const CardClothes = () => {
                   d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
                 />
               </svg>{" "}
-              Username
+              {props.Username}
             </a>
           </div>
 
@@ -90,7 +90,7 @@ const CardClothes = () => {
                   d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
                 />
               </svg>
-              149km
+              {props.distance}
             </span>
           </div>
         </div>
--- src/Pages/browse.jsx
+++ src/Pages/browse.jsx
@@ -1,18 +1,65 @@
-// import { NavLink } from "react-router-dom";
-import Location from "../Components/location";
+import React from "react";
+import CardClothes from "../Components/CardClothes";
+import ImageSlider from "../Components/Carousals/ImageSlider";
 import Navbar from "../Components/Navbar";
-// import CarousalImage from "../Components/Carousals/CarousalImage";
-function Browse() {
+import Location from "../Components/location";
+import Button from "../Components/modal/button";
+
+const Browse = () => {
+  const slides = [
+    {
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSimMDuMEMZWRWmAqL1It9mF1Nw1o_iMY2UVQ&usqp=CAU",
+    },
+    {
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBNnH7hPrYmvHPtcImonVIDwGgMIgtg5URew&usqp=CAU",
+    },
+    {
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSudU6gmyeog5RWlz8b2FUxARwyrD-GMo9txw&usqp=CAU",
+    },
+    {
+      url: "https://e0.pxfuel.com/wallpapers/378/1016/desktop-wallpaper-obito-uchiha-anime-naruto-shippuden-naruto.jpg",
+    },
+    {
+      url: "https://e0.pxfuel.com/wallpapers/34/167/desktop-wallpaper-cute-naruto-anime-naruto-little-naruto-uzumaki.jpg",
+    },
+  ];
+
+  const containerStyles = {
+    width: "100%",
+    height: "300px",
+    margin: "0 auto",
+  };
+
+  const cardClothesData = [
+    { userName: "User123", distance: 120 },
+    { userName: "Fashionista", distance: 240 },
+    { userName: "Trendsetter", distance: 50 },
+  ];
+
   return (
     <div className="flex flex-row">
       <Navbar />
-      <div className="flex">
+      <div>
         <Location />
-        {/* <CarousalImage /> */}
+        <div style={containerStyles}>
+          <ImageSlider slides={slides} />
+        </div>
+        <div className="m-10 flex flex-col">
+          <Button />
+          <hr className="" />
+        </div>
+        <div>
+          <ul className="grid grid-cols-3 lg:grid-cols-3 sm:grid-cols-1">
+            {cardClothesData.map((data, index) => (
+              <li key={index} className="m-5">
+                <CardClothes userName={data.userName} distance={data.distance} />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      {/* <CarousalImage /> */}
     </div>
   );
-}
+};
 
 export default Browse;
