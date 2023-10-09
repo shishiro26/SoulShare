655766d1cf9fa45f00e4509de800dd1c6b0b5668
shishiro <dheervathshishiro@gmail.com>
2023-10-10 01:12:03 +0530

    added backend

--- server/app.js
+++ server/app.js
@@ -28,11 +28,14 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("dev")); //logs the requests with some information
 app.use(express.urlencoded({ extended: true })); // Handle form data
-app.use(cors(
-  {
-    origin: 'http://localhost:5173',
-    credentials: true
-  }));
+// app.use(cors(
+//   {
+//     origin: 'http://localhost:5173',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//     optionsSuccessStatus: 204,
+//   }));
+app.use(cors())
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
